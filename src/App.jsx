9cb1904253e70b87bceee8830b638f820bfad957
unknown <elmurodvkh@gmail.com>
2024-12-16 10:04:24 +0500

    setup

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function App() {
+  return (
+    <main>
+      <h1 className='text-5xl text-orange-500 font-bold'>Welcome to Awwwards</h1>
+    </main>
+  )
+}
