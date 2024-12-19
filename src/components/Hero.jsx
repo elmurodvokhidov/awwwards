9f4be74aa091bb035d071747f0fd5e54bb9b3c71
unknown <elmurodvkh@gmail.com>
2024-12-19 22:39:00 +0500

    hero section #2

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,4 +1,6 @@
 import { useRef, useState } from "react"
+import Button from "./Button";
+import { TiLocationArrow } from "react-icons/ti";
 
 export default function Hero() {
     const [currentIndex, setCurrentIndex] = useState(1);
@@ -6,12 +8,13 @@ export default function Hero() {
     const [isLoading, setIsLoading] = useState(false);
     const [loadedVideos, setLoadedVideos] = useState(0);
 
-    const totalVideos = 4;
+    const totalVideos = 3;
     const nextVdRef = useRef(null);
+    const upcomingVdIndex = (currentIndex % totalVideos) + 1;
 
     const handleMiniVdClick = () => {
         setHasClicked(true);
-        setCurrentIndex(prev => prev + 1);
+        setCurrentIndex(upcomingVdIndex);
     }
 
     const getVdSrc = (index) => `videos/hero-${index}.mp4`;
@@ -28,7 +31,7 @@ export default function Hero() {
                         <div onClick={handleMiniVdClick} className="origin-center scale-50 opacity-0 transition-all duration-500 ease-in hover:scale-100 hover:opacity-100">
                             <video
                                 ref={nextVdRef}
-                                src={getVdSrc(currentIndex + 1)}
+                                src={getVdSrc(upcomingVdIndex)}
                                 loop
                                 muted
                                 id="current-video"
@@ -37,8 +40,54 @@ export default function Hero() {
                             />
                         </div>
                     </div>
+
+                    <video
+                        ref={nextVdRef}
+                        src={getVdSrc(currentIndex)}
+                        loop
+                        muted
+                        id="next-video"
+                        className="absolute-center invisible absolute z-20 size-64 object-cover object-center"
+                        onLoadedData={handleVdLoad}
+                    />
+
+                    <video
+                        src={getVdSrc(currentIndex === totalVideos - 1 ? 1 : currentIndex)}
+                        autoPlay
+                        loop
+                        muted
+                        className="absolute left-0 top-0 size-full object-cover object-center"
+                        onLoadedData={handleVdLoad}
+                    />
+                </div>
+
+                <h1 className="special-font hero-heading absolute bottom-5 right-5 z-40 text-blue-75">
+                    G<b>a</b>ming
+                </h1>
+
+                <div className="absolute left-0 top-0 z-40 size-full">
+                    <div className="mt-24 px-5 sm:px-10">
+                        <h1 className="special-font hero-heading text-blue-100">
+                            redefi<b>n</b>e
+                        </h1>
+
+                        <p className="mb-5 max-w-64 font-robert-regular text-blue-100">
+                            Enter the Metagame Layer <br /> Unleash the Play Economy
+                        </p>
+
+                        <Button
+                            id="watch-trailer"
+                            title="Watch Trailer"
+                            leftIcon={<TiLocationArrow />}
+                            containerClass="!bg-yellow-300 flex-center gap-1"
+                        />
+                    </div>
                 </div>
             </div>
+
+            <h1 className="special-font hero-heading absolute bottom-5 right-5 text-black">
+                G<b>a</b>ming
+            </h1>
         </div>
     )
 }
