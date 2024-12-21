787cd33259b341045b8a9184f2d636d419274eeb
unknown <elmurodvkh@gmail.com>
2024-12-21 22:28:53 +0500

    about section

---
+++ src/components/AnimatedTitle.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function AnimatedTitle() {
+    return (
+        <div className="mt-5 text-center text-4xl uppercase leading-[0.8] md:text-[6rem]">
+            Disc<b>o</b>ver the world's <br />
+            l<b>a</b>rgest shared adventure
+        </div>
+    )
+}
