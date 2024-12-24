cdc55add768220f89f61da3a5169620c59a6a965
unknown <elmurodvkh@gmail.com>
2024-12-24 12:02:29 +0500

    navbar #1

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import Hero from './components/Hero'
 import About from './components/About'
+import Navbar from './components/Navbar'
 
 export default function App() {
   return (
     <main className="relative min-h-screen w-screen overflow-x-hidden">
+      <Navbar />
+
       <Hero />
 
       <About />
