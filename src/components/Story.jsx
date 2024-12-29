904d7e25a182f0726e8e58a79965e6906e7c1f71
unknown <elmurodvkh@gmail.com>
2024-12-29 21:21:36 +0500

    story #2

--- src/components/Story.jsx
+++ src/components/Story.jsx
@@ -1,11 +1,24 @@
 import React, { useRef } from 'react'
 import AnimatedTitle from './AnimatedTitle'
 import gsap from 'gsap';
+import RoundedCorners from './RoundedCorners';
+import Button from './Button';
 
 export default function Story() {
     const frameRef = useRef(null);
 
-    const handleMouseLeave = () => { }
+    const handleMouseLeave = () => {
+        const element = frameRef.current;
+
+        if (element) {
+            gsap.to(element, {
+                duration: 0.3,
+                rotateX: 0,
+                rotateY: 0,
+                ease: "power1.inOut",
+            });
+        }
+    }
 
     const handleMouseMove = (e) => {
         const { clientX, clientY } = e;
@@ -38,6 +51,7 @@ export default function Story() {
                 <p className="font-general text-sm uppercase md:text-[10px]">
                     the multiversal ip world
                 </p>
+
                 <div className="relative size-full">
                     <AnimatedTitle
                         title="the st<b>o</b>ry of <br /> a hidden real<b>m</b>"
@@ -60,31 +74,23 @@ export default function Story() {
                             </div>
                         </div>
 
-                        <svg
-                            className="invisible absolute size-0"
-                            xmlns="http://www.w3.org/2000/svg"
-                        >
-                            <defs>
-                                <filter id="flt_tag">
-                                    <feGaussianBlur
-                                        in="SourceGraphic"
-                                        stdDeviation="8"
-                                        result="blur"
-                                    />
-                                    <feColorMatrix
-                                        in="blur"
-                                        mode="matrix"
-                                        values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 19 -9"
-                                        result="flt_tag"
-                                    />
-                                    <feComposite
-                                        in="SourceGraphic"
-                                        in2="flt_tag"
-                                        operator="atop"
-                                    />
-                                </filter>
-                            </defs>
-                        </svg>
+                        <RoundedCorners />
+                    </div>
+                </div>
+
+                <div className="-mt-80 flex w-full justify-center md:-mt-64 md:me-44 md:justify-end">
+                    <div className="flex h-full w-fit flex-col items-center md:items-start">
+                        <p className="mt-3 max-w-sm text-center font-circular-web text-violet-50 md:text-start">
+                            Where realms converge, lies Zentry and the boundless pillar.
+                            Discover its secrets and shape your fate amidst infinite
+                            opportunities.
+                        </p>
+
+                        <Button
+                            id="realm-btn"
+                            title="discover prologue"
+                            containerClass="mt-5"
+                        />
                     </div>
                 </div>
             </div>
